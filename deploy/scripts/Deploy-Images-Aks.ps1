39570352c459362ce3edddcb133affd8cbb7ad19
Matthew Alan Gray <matthew.gray@hatboysoftware.com>
2023-10-03 13:47:26 -0500

    Deploying LangChainAPI via helm to AKS with appropriate managed identity access to KeyVault and Storage Account

--- deploy/scripts/Deploy-Images-Aks.ps1
+++ deploy/scripts/Deploy-Images-Aks.ps1
@@ -116,6 +116,13 @@ if ([String]::IsNullOrEmpty($valuesFile)) {
 
 Write-Host "Configuration file used is $valuesFile" -ForegroundColor Yellow
 
+if ($charts.Contains("langchain-api") -or  $charts.Contains("*")) {
+    Write-Host "API chart - api" -ForegroundColor Yellow
+    $command = "helm upgrade --install $name-lc-api ./langchain-api -f $valuesFile --set ingress.hosts='{$aksHost}' --set image.repository=$acrLogin/langchain-api --set image.tag=$tag --set hpa.activated=$autoscale"
+    $command = createHelmCommand $command 
+    Invoke-Expression "$command"
+}
+
 if ($charts.Contains("chat-api") -or  $charts.Contains("*")) {
     Write-Host "API chart - api" -ForegroundColor Yellow
     $command = "helm upgrade --install $name-api ./chat-api -f $valuesFile --set ingress.hosts='{$aksHost}' --set image.repository=$acrLogin/chat-api --set image.tag=$tag --set hpa.activated=$autoscale"
