4bc4ca344dc0de76b6f1f4f98ea94714aaba15fc
Jim Counts <jamesrcounts@outlook.com>
2023-10-20 15:56:39 +0000

    Stub files

--- deploy/tf/ops/versions.tf
+++ deploy/tf/ops/versions.tf
@@ -1,19 +1,3 @@
 terraform {
-    required_providers {
-        azurerm = {
-            source = "hashicorp/azurerm"
-            version = "3.65.0"
-        }
-    }
-
-    cloud {
-        organization = "FoundationaLLM"
-        workspaces {
-            name = "FoundationaLLM-OPS"
-        }
-    }
-}
-
-provider "azurerm" {
-    features {}
+  required_version = "~> 1.3"
 }
