e91de4c34cff877f3dbae3aaf3413ce5474df033
Jim Counts <jamesrcounts@outlook.com>
2023-10-27 22:32:25 +0000

    Adjust AGW Outputs

--- deploy/tf/fllm/modules/application-gateway/outputs.tf
+++ deploy/tf/fllm/modules/application-gateway/outputs.tf
@@ -5,5 +5,5 @@ output "id" {
 
 output "identity_id" {
   description = "The Application Gateway identity."
-  value       = azurerm_application_gateway.main.identity[0]
+  value       = tolist(azurerm_application_gateway.main.identity[0].identity_ids)[0]
 }
