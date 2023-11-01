85a79012f48714402c388bcbb87a7b65fcf957bf
Jim Counts <jamesrcounts@outlook.com>
2023-11-01 18:55:40 +0000

    Allow Backend to Access Vectorization

--- deploy/tf/ops/main.tf
+++ deploy/tf/ops/main.tf
@@ -393,7 +393,17 @@ locals {
       address_prefix = cidrsubnet(local.network_cidr, 8, 6)
 
       nsg_rules = {
-        inbound  = merge(local.default_nsg_rules.inbound, {})
+        inbound = merge(local.default_nsg_rules.inbound, {
+          "allow-aks-inbound" = {
+            access                     = "Allow"
+            destination_address_prefix = "VirtualNetwork"
+            destination_port_range     = "*"
+            priority                   = 256
+            protocol                   = "*"
+            source_address_prefixes    = [local.address_prefix["fllm_backend"]]
+            source_port_range          = "*"
+          }
+        })
         outbound = merge(local.default_nsg_rules.outbound, {})
       }
     }
