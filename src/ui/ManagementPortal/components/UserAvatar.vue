7663fc07f9a9ab951b22b47eb0f13133f6bbd3e2
Sensational Code <sensational.code@gmail.com>
2024-09-18 07:10:28 -0700

    Use UserAvatar component on management portal

---
+++ src/ui/ManagementPortal/components/UserAvatar.vue
@@ -0,0 +1,101 @@
+<template>
+	<!-- <Avatar v-if="loading" icon="pi pi-spinner" /> -->
+	<Avatar v-if="profilePhotoSrc" :image="profilePhotoSrc" />
+	<Avatar v-else :label="userInitials" :style="`background-color: ${userColors.background}; color: ${userColors.text};`" />
+</template>
+
+<script lang="ts">
+const paleColors = [
+	'#FFFFCC', // Pale Yellow
+	'#FFCCCC', // Pale Red
+	'#FFCCFF', // Pale Pink
+	'#CCFFFF', // Pale Cyan
+	'#CCFFCC', // Pale Green
+	'#CCCCFF', // Pale Lavender
+	'#FFCC99', // Pale Peach
+	'#FF9966', // Pale Orange
+	'#FF9999', // Pale Salmon
+	'#99CCFF'  // Pale Sky Blue
+];
+
+// const darkPaleColors = [
+//   '#CCCC99', // Dark Pale Yellow
+//   '#CC9999', // Dark Pale Red
+//   '#CC99CC', // Dark Pale Pink
+//   '#99CCCC', // Dark Pale Cyan
+//   '#99CC99', // Dark Pale Green
+//   '#9999CC', // Dark Pale Lavender
+//   '#CC9966', // Dark Pale Peach
+//   '#CC6633', // Dark Pale Orange
+//   '#CC6666', // Dark Pale Salmon
+//   '#6699CC'  // Dark Pale Sky Blue
+// ];
+
+const darkerPaleColors = [
+	'#999966', // Darker Pale Yellow
+	'#993333', // Darker Pale Red
+	'#993399', // Darker Pale Pink
+	'#339999', // Darker Pale Cyan
+	'#339933', // Darker Pale Green
+	'#333399', // Darker Pale Lavender
+	'#993300', // Darker Pale Peach
+	'#663300', // Darker Pale Orange
+	'#993333', // Darker Pale Salmon
+	'#336699'  // Darker Pale Sky Blue
+];
+
+function hashString(str) {
+	let hash = 0;
+	for (let i = 0; i < str.length; i++) {
+		hash = str.charCodeAt(i) + ((hash << 5) - hash);
+	}
+	return hash;
+}
+
+function getColorsFromName(name) {
+	const hash = hashString(name);
+	const index = Math.abs(hash) % paleColors.length;
+	return {
+		base: paleColors[index],
+		text: darkerPaleColors[index],
+	};
+}
+
+function getInitials(text) {
+	const words = text.split(/\s+/);
+	return (words[0][0] + (words[words.length - 1][0] || '')).toUpperCase();
+}
+
+export default {
+	data() {
+		return {
+			// loading: false,
+			profilePhotoSrc: null,
+		};
+	},
+
+	computed: {
+		userInitials() {
+			return getInitials(this.$authStore.currentAccount?.name);
+		},
+
+		userColors() {
+			const colors = getColorsFromName(this.$authStore.currentAccount?.name);
+			return {
+				text: colors.text,
+				background: colors.base,
+			};
+		},
+	},
+
+	async created() {
+		// this.loading = true;
+		try {
+			this.profilePhotoSrc = await this.$authStore.getProfilePhoto();
+		} catch(error) {
+			// do something
+		}
+		// this.loading = false;
+	},
+};
+</script>
