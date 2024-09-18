1dcbccf35747cf6c1f34baaf7ff06dc5646de0af
Sensational Code <sensational.code@gmail.com>
2024-09-18 08:18:25 -0700

    Increase user avatar initials contrast

--- src/ui/UserPortal/components/UserAvatar.vue
+++ src/ui/UserPortal/components/UserAvatar.vue
@@ -18,7 +18,7 @@ const paleColors = [
 	'#99CCFF'  // Pale Sky Blue
 ];
 
-// const darkPaleColors = [
+// const paleColorsDarken1 = [
 //   '#CCCC99', // Dark Pale Yellow
 //   '#CC9999', // Dark Pale Red
 //   '#CC99CC', // Dark Pale Pink
@@ -31,17 +31,30 @@ const paleColors = [
 //   '#6699CC'  // Dark Pale Sky Blue
 // ];
 
-const darkerPaleColors = [
-	'#999966', // Darker Pale Yellow
-	'#993333', // Darker Pale Red
-	'#993399', // Darker Pale Pink
-	'#339999', // Darker Pale Cyan
-	'#339933', // Darker Pale Green
-	'#333399', // Darker Pale Lavender
-	'#993300', // Darker Pale Peach
-	'#663300', // Darker Pale Orange
-	'#993333', // Darker Pale Salmon
-	'#336699'  // Darker Pale Sky Blue
+// const paleColorsDarken2 = [
+// 	'#999966', // Darker Pale Yellow
+// 	'#993333', // Darker Pale Red
+// 	'#993399', // Darker Pale Pink
+// 	'#339999', // Darker Pale Cyan
+// 	'#339933', // Darker Pale Green
+// 	'#333399', // Darker Pale Lavender
+// 	'#993300', // Darker Pale Peach
+// 	'#663300', // Darker Pale Orange
+// 	'#993333', // Darker Pale Salmon
+// 	'#336699'  // Darker Pale Sky Blue
+// ];
+
+const paleColorsDarken3 = [
+	'#666633', // Much Darker Pale Yellow
+	'#993333', // Much Darker Pale Red
+	'#993399', // Much Darker Pale Pink
+	'#336666', // Much Darker Pale Cyan
+	'#336633', // Much Darker Pale Green
+	'#333366', // Much Darker Pale Lavender
+	'#993300', // Much Darker Pale Peach
+	'#663300', // Much Darker Pale Orange
+	'#993333', // Much Darker Pale Salmon
+	'#336699'  // Much Darker Pale Sky Blue
 ];
 
 function hashString(str) {
@@ -57,7 +70,7 @@ function getColorsFromName(name) {
 	const index = Math.abs(hash) % paleColors.length;
 	return {
 		base: paleColors[index],
-		text: darkerPaleColors[index],
+		text: paleColorsDarken3[index],
 	};
 }
 
