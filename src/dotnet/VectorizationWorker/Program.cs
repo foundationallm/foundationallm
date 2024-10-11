d4c5bb915e7a554b7dceeaa8c6dcdc273bc5e0b7
Chris Givens <givenscj@Hotmail.com>
2024-10-11 09:54:08 -0400

    Update Program.cs

--- src/dotnet/VectorizationWorker/Program.cs
+++ src/dotnet/VectorizationWorker/Program.cs
@@ -136,7 +136,6 @@ builder.Services.AddKeyedSingleton<IStorageService, BlobStorageService>(
     });
 
 // Vectorization state
-builder.Services.AddSingleton<MemoryVectorizationStateService, MemoryVectorizationStateService>(); //for sync requests
 builder.Services.AddSingleton<IVectorizationStateService, BlobStorageVectorizationStateService>();
 
 // Resource validation
