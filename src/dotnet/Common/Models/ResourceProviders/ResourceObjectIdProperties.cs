5377fa15bcb4216e23c6d0df79054abebae2b6c9
gabrielgheorghescu <gabriel@jichiciconsulting.eu>
2024-12-06 14:23:16 +0200

    Extend agent tool configuration

---
+++ src/dotnet/Common/Models/ResourceProviders/ResourceObjectIdProperties.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FoundationaLLM.Common.Models.ResourceProviders
+{
+    /// <summary>
+    /// Defines the properties of a resource.
+    /// </summary>
+    public class ResourceObjectIdProperties
+    {
+        /// <summary>
+        /// The unique identifier of the resource.
+        /// </summary>
+        [JsonPropertyName("object_id")]
+        public required string ObjectId { get; set; }
+
+        /// <summary>
+        /// Gets or sets a dictionary of properties.
+        /// </summary>
+        [JsonPropertyName("properties")]
+        public Dictionary<string, object> Properties { get; set; } = [];
+    }
+}
