6f16c50526e5d1c78a2a4da630201d8537fd4700
Sensational Code <sensational.code@gmail.com>
2024-12-11 08:20:14 -0800

    Add more utility classes

--- src/ui/ManagementPortal/app.vue
+++ src/ui/ManagementPortal/app.vue
@@ -100,6 +100,10 @@ body,
 	margin-left: 4px !important;
 }
 
+.ml-2 {
+	margin-left: 8px !important;
+}
+
 .mr-1 {
 	margin-right: 4px !important;
 }
@@ -122,6 +126,14 @@ body,
 
 .mb-4 {
 	margin-bottom: 16px !important;
+}
+
+.mb-5 {
+	margin-bottom: 20px !important;
+}
+
+.mb-6 {
+	margin-bottom: 24px !important;
 }
 
 .w-100 {
