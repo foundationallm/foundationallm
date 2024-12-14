a6b9519f802190701fbf1e7d51c64f82a9ae6702
Carey Payette <carey.payette@gmail.com>
2024-12-14 11:51:14 -0500

    Prompt token replacement alternative solution

---
+++ src/dotnet/Common/Constants/ResourceProviders/PromptTokens.cs
@@ -0,0 +1,13 @@
+﻿namespace FoundationaLLM.Common.Constants.ResourceProviders
+{
+    /// <summary>
+    /// Set of well-known tokens that can be used in prompts.
+    /// </summary>
+    public static class PromptTokens
+    {
+        /// <summary>
+        /// Token for current date in UTC format.
+        /// </summary>
+       public const string CurrentDateUTC = "{{current_date_utc}}";       
+    }
+}
---
+++ src/dotnet/Common/Services/TokenReplacement/TokenReplacementEngine.cs
@@ -0,0 +1,29 @@
+﻿using FoundationaLLM.Common.Constants.ResourceProviders;
+
+namespace FoundationaLLM.Common.Services.TokenReplacement
+{
+    /// <summary>
+    /// Engine for replacing tokens in strings.
+    /// </summary>
+    public class TokenReplacementEngine
+    {
+        /// <summary>
+        /// Replaces tokens in the input string with the corresponding values.
+        /// </summary>
+        /// <param name="input">The string from which to replace the tokens.</param>
+        /// <returns>Input string with tokens replaced.</returns>
+        public static string ReplaceTokens(string input)
+        {
+            if(string.IsNullOrWhiteSpace(input))
+            {
+                return string.Empty;
+            }
+            if (input.Contains(PromptTokens.CurrentDateUTC))
+            {
+                input = input.Replace(PromptTokens.CurrentDateUTC, DateTime.UtcNow.ToString("dddd, MMMM dd, yyyy"));
+            }
+            return input;
+            
+        }
+    }
+}
