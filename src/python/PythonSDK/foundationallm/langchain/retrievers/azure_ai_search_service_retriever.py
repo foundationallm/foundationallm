e80ae81566dfcc30d212146a12a42c38b217863f
Carey Payette <carey.payette@gmail.com>
2024-12-15 18:27:58 -0500

    Fixed rerank property name in AI Search

--- src/python/PythonSDK/foundationallm/langchain/retrievers/azure_ai_search_service_retriever.py
+++ src/python/PythonSDK/foundationallm/langchain/retrievers/azure_ai_search_service_retriever.py
@@ -116,9 +116,9 @@ class AzureAISearchServiceRetriever(BaseRetriever, ContentArtifactRetrievalBase)
                         page_content=result[index_config.indexing_profile.settings.text_field_name],
                         metadata=metadata,
                         score=result["@search.score"],
-                        rerank_score=result.get("@search.rerankScore", 0.0)
+                        rerank_score=result.get("@search.rerankerScore", 0.0)
                 )
-                if('@search.rerankScore' in result):                    
+                if('@search.rerankerScore' in result):                    
                     rerank_available = True
                     
                 document.score = result["@search.score"]
