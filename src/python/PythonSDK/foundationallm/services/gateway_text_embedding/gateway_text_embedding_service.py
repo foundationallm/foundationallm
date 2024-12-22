96fe9a376ff4e577474f29a125cd87f57b88cdf3
Chris Givens <givenscj@hotmail.com>
2024-12-22 12:03:54 -0500

    Check for null or empty embedding request

--- src/python/PythonSDK/foundationallm/services/gateway_text_embedding/gateway_text_embedding_service.py
+++ src/python/PythonSDK/foundationallm/services/gateway_text_embedding/gateway_text_embedding_service.py
@@ -1,5 +1,5 @@
 """
-Class: GatewayTextEmbeddingService  
+Class: GatewayTextEmbeddingService
 Description:  Class responsible for obtaining text embedding vectors from the Gateway API.
 """
 import asyncio
@@ -22,28 +22,32 @@ class GatewayTextEmbeddingService():
                  gateway_api_endpoint_configuration: APIEndpointConfiguration,
                  model_name:str,
                  config: Configuration):
-        self.http_client = HttpClientService(gateway_api_endpoint_configuration, user_identity, config)       
+        self.http_client = HttpClientService(gateway_api_endpoint_configuration, user_identity, config)
         self.model_name = model_name
         self.config = config
         self.url =  f'/instances/{instance_id}/embeddings'
-        
+
     def get_embedding(self, text: str) -> GatewayTextEmbeddingResponse:
         """
         Get the embedding vector for a given text.
-        """        
-        # create the text embedding request        
+        """
+
+        if (text == None) or (len(text) == 0):
+            raise Exception("Text is empty or None")
+
+        # create the text embedding request
         text_embedding_request = self._create_text_embedding_request(text)
-        
+
         # start the operation
         request_json = text_embedding_request.model_dump_json(by_alias=True)
-        
-        resp = self.http_client.post(self.url, data=request_json)        
-        response = TextEmbeddingResponse.model_validate(resp)        
+
+        resp = self.http_client.post(self.url, data=request_json)
+        response = TextEmbeddingResponse.model_validate(resp)
         # poll until completion
-        while response.in_progress and not response.failed:            
-            # delay for a second in between polling            
+        while response.in_progress and not response.failed:
+            # delay for a second in between polling
             time.sleep(1)
-            get_resp = self.http_client.get(self.url + f'?operationId={response.operation_id}')            
+            get_resp = self.http_client.get(self.url + f'?operationId={response.operation_id}')
             response = TextEmbeddingResponse.model_validate(get_resp)
 
         if response.failed:
@@ -57,7 +61,7 @@ class GatewayTextEmbeddingService():
         """
         Asynchronously get the embedding vector for a given text.
         """
-        
+
         text_embedding_request = self._create_text_embedding_request(text)
 
         # start the operation
@@ -65,12 +69,12 @@ class GatewayTextEmbeddingService():
 
         # Send asynchronous POST request to start the operation
         response = TextEmbeddingResponse.model_validate(await self.http_client.apost(self.url, data=request_json))
-        
+
         # Poll until operation is complete
         while response.in_progress and not response.failed:
             await asyncio.sleep(1)  # Use asyncio.sleep for non-blocking delay
             response = TextEmbeddingResponse.model_validate(await self.http_client.aget(self.url + f'?operationId={response.operation_id}'))
-        
+
         if response.failed:
             raise Exception(f"Text embedding operation failed: {response.error_message}")
 
