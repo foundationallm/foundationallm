ae00e743baecfe3e64c8a41d4b7d5a923d91e2ca
Ciprian Jichici <ciprian@solliance.net>
2025-01-14 15:29:01 +0200

    Fix issue with cache settings and add content artifacts to message history

--- src/dotnet/Common/Models/ResourceProviders/Agent/AgentConversationHistorySettings.cs
+++ src/dotnet/Common/Models/ResourceProviders/Agent/AgentConversationHistorySettings.cs
@@ -18,5 +18,11 @@ namespace FoundationaLLM.Common.Models.ResourceProviders.Agent
         /// </summary>
         [JsonPropertyName("max_history")]
         public int MaxHistory { get; set; }
+
+        /// <summary>
+        /// Gets or sets the comma separated list of content artifact types to store in the conversation history.
+        /// </summary>
+        [JsonPropertyName("history_content_artifact_types")]
+        public string? HistoryContentArtifactTypes { get; set; }
     }
 }
