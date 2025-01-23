ee5aae631e0a11d66d3f7384308f62b32fa93028
joelhulen <joelhulen@gmail.com>
2025-01-23 14:23:54 -0500

    Update content artifact styling

--- src/ui/UserPortal/components/ChatMessage.vue
+++ src/ui/UserPortal/components/ChatMessage.vue
@@ -921,6 +921,10 @@ $textColor: #131833;
 	padding: 4px 8px;
 	cursor: pointer;
 	white-space: nowrap;
+	max-width: 20rem;
+	white-space: nowrap;
+	overflow: hidden;
+	text-overflow: ellipsis;
 }
 
 .ratings {
