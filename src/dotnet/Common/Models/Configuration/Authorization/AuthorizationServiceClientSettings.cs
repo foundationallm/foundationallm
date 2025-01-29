08dba75bca8c925aac4f5c2d9f69b530da567751
joelhulen <joelhulen@gmail.com>
2025-01-29 13:16:30 -0500

    Tweak data types and udpate comments

--- src/dotnet/Common/Models/Configuration/Authorization/AuthorizationServiceClientSettings.cs
+++ src/dotnet/Common/Models/Configuration/Authorization/AuthorizationServiceClientSettings.cs
@@ -23,20 +23,20 @@
         /// <summary>
         /// Absolute cache expiration in seconds.
         /// </summary>
-        public int? AbsoluteCacheExpirationSeconds { get; set; } = 300;
+        public double? AbsoluteCacheExpirationSeconds { get; set; } = 300;
 
         /// Sets how many seconds the cache entry can be inactive (e.g. not accessed) before it will be removed.
         /// This will not extend the entry lifetime beyond the absolute expiration (if set).
-        public int? SlidingCacheExpirationSeconds { get; set; } = 120;
+        public double? SlidingCacheExpirationSeconds { get; set; } = 120;
 
         /// <summary>
         /// The maximum number of items that can be stored in the cache.
         /// </summary>
-        public long? CacheSizeLimit { get; set; } = 1000;
+        public long? CacheSizeLimit { get; set; } = 10000;
 
         /// <summary>
         /// Gets or sets the minimum length of time between successive scans for expired items in seconds.
         /// </summary>
-        public int? CacheExpirationScanFrequencySeconds { get; set; } = 30;
+        public double? CacheExpirationScanFrequencySeconds { get; set; } = 30;
     }
 }
