c4efa6f49f430ea9473031ceba3e3364a0b13f75
Matthew Alan Gray <mgray@solliance.net>
2025-02-28 19:30:16 +0000

    Adding support for escrowed docker images and helm charts

--- deploy/standard/azd-hooks/utility/Deploy-Frontend-Aks.ps1
+++ deploy/standard/azd-hooks/utility/Deploy-Frontend-Aks.ps1
@@ -8,7 +8,8 @@ Param(
     [parameter(Mandatory = $false)][string]$secretProviderClassManifest,
     [parameter(Mandatory = $false)][string]$serviceNamespace = "fllm",
     [parameter(Mandatory = $false)][string]$registry = "ghcr.io/solliancenet",
-    [parameter(Mandatory = $false)][string]$version = "0.7.0"
+    [parameter(Mandatory = $false)][string]$version = "0.7.0",
+    [parameter(Mandatory = $false)][string]$ingressEscrowed = $false
 )
 
 Set-PSDebug -Trace 0 # Echo every command (0 to disable, 1 to enable, 2 to enable verbose)
