e1d4d67c14289e02babeb26a715d694ea511d217
Carey Payette <carey.payette@gmail.com>
2025-03-12 16:21:58 -0400

    Added prompt builder and token replacement functionality

--- src/dotnet/Common/Constants/Templates/TemplateVariables.cs
+++ src/dotnet/Common/Constants/Templates/TemplateVariables.cs
@@ -9,5 +9,15 @@
         /// Token for current date in UTC format.
         /// </summary>
         public const string CurrentDateTimeUTC = "current_datetime_utc";
+
+        /// <summary>
+        /// Token for a workflow router prompt.
+        /// </summary>
+        public const string RouterPrompt = "router_prompt";
+
+        /// <summary>
+        /// Token for the router prompt tools list.
+        /// </summary>
+        public const string PromptToolsList = "prompt_tools_list";
     }
 }
--- src/dotnet/Common/Interfaces/ITemplatingService.cs
+++ src/dotnet/Common/Interfaces/ITemplatingService.cs
@@ -9,7 +9,8 @@
         /// Transforms the input string by replacing tokens with the corresponding values.
         /// </summary>
         /// <param name="s">The input string to be transformed.</param>
+        /// <param name="tokenAndValues">Optional dictionary of well-known tokens and the values that should take its place.</param>
         /// <returns>The transformed string where all the valid tokens have been replaced.</returns>
-        string Transform(string s);
+        string Transform(string s, Dictionary<string, string>? tokenAndValues = null);
     }
 }
