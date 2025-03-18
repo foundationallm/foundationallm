0d9105592aed17617c81e2914b48c13b10b26213
Sensational Code <sensational.code@gmail.com>
2025-03-18 12:24:51 -0700

    Manually fix lint errors

--- src/ui/ManagementPortal/components/PropertyBuilder.vue
+++ src/ui/ManagementPortal/components/PropertyBuilder.vue
@@ -88,7 +88,7 @@
 				v-model="propertyToEdit"
 				:title="propertyToEdit ? 'Edit Property' : 'Create Property'"
 				:visible="showCreateOrEditPropertyDialog"
-				@update:modelValue="handleAddProperty($event)"
+				@update:model-value="handleAddProperty($event)"
 				@update:visible="handleClosePropertyDialog"
 			/>
 
