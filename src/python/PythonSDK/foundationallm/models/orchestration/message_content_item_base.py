3d21d7fb8cff94deb938c710aacd5a894af4addc
Carey Payette <carey.payette@gmail.com>
2025-03-25 18:56:48 -0400

    Adds text property to MessageContentItemBase in Python

--- src/python/PythonSDK/foundationallm/models/orchestration/message_content_item_base.py
+++ src/python/PythonSDK/foundationallm/models/orchestration/message_content_item_base.py
@@ -7,6 +7,7 @@ class MessageContentItemBase(BaseModel):
     """Base message content item model."""
 
     type: Optional[str] = Field(None, description="The type of message content item.")
+    text: Optional[str] = Field(None, alias="text")
     agent_capability_category: Optional[AgentCapabilityCategories] = Field(None, description="The category of capability assigned to the agent.")
     
     class Config:
