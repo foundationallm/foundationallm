fa4f212a13524595174b15f42e7596b934c639bd
Matthew Gray <matthew.gray@hatboysoftware.com>
2025-03-26 09:29:47 -0500

    Updating ingress-nginx to 4.12.1

--- deploy/standard/azd-hooks/utility/Deploy-Frontend-Aks.ps1
+++ deploy/standard/azd-hooks/utility/Deploy-Frontend-Aks.ps1
@@ -65,7 +65,7 @@ Invoke-AndRequireSuccess "Deploy ingress-nginx" {
         --install gateway ingress-nginx/ingress-nginx `
         --namespace ${gatewayNamespace} `
         --values ${ingressNginxValues} `
-        --version 4.10.0
+        --version 4.12.1
 }
 
 # **** Service Namespace ****
