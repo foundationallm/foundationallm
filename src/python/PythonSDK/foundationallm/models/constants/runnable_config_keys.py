98bea50f98c4641466dfb7c64af9b2f62e5581d9
Carey Payette <carey.payette@gmail.com>
2025-04-01 23:47:18 -0400

    Change RunnableConfigKeys from an enum to constants

--- src/python/PythonSDK/foundationallm/models/constants/runnable_config_keys.py
+++ src/python/PythonSDK/foundationallm/models/constants/runnable_config_keys.py
@@ -1,6 +1,4 @@
-from enum import Enum
-
-class RunnableConfigKeys(str, Enum):
+class RunnableConfigKeys:
     """The keys for RunnableConfig values."""
     ORIGINAL_USER_PROMPT = 'original_user_prompt'
     ORIGINAL_USER_PROMPT_REWRITE = 'original_user_prompt_rewrite'
