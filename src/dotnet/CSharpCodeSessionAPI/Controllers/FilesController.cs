7529b01660c9ab838a5f5cb84a2fe25bc43f1394
Ciprian Jichici <ciprian@solliance.net>
2025-04-10 18:31:38 +0300

    CSharp code session custom container

---
+++ src/dotnet/CSharpCodeSessionAPI/Controllers/FilesController.cs
@@ -0,0 +1,35 @@
+﻿using FoundationaLLM.CSharpCodeSession.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoundationaLLM.CSharpCodeSession.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilesController(ILogger<FilesController> logger) : ControllerBase
+    {
+        private readonly ILogger<FilesController> _logger = logger;
+
+        private const string ROOT_PATH = "\\mnt\\data";
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadFile([FromForm] IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return BadRequest("No file uploaded.");
+
+            var filePath = NormalizePath(Path.Combine(ROOT_PATH, file.FileName));
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
+            return Ok(new UploadFileResponse
+            {
+                FileName = Path.GetFileName(filePath)
+            });
+        }
+
+        private static string NormalizePath(string path) =>
+            Path.GetFullPath(new Uri(path).LocalPath)
+                .TrimEnd(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
+    }
+}
---
+++ src/dotnet/CSharpCodeSessionAPI/Models/UploadFileResponse.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FoundationaLLM.CSharpCodeSession.API.Models
+{
+    /// <summary>
+    /// Response model for file upload.
+    /// </summary>
+    public class UploadFileResponse
+    {
+        /// <summary>
+        /// Gets or sets the status of the upload operation.
+        /// </summary>
+        [JsonPropertyName("status")]
+        public string Status { get; set; } = "success";
+
+        /// <summary>
+        /// Gets or sets the name of the file that was uploaded.
+        /// </summary>
+        [JsonPropertyName("file_name")]
+        public required string FileName { get; set; }
+    }
+}
