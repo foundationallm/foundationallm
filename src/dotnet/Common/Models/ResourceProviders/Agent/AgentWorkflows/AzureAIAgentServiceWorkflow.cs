c24227a90afd1eafd43311733e5041cb12f2db15
Carey Payette <carey.payette@gmail.com>
2025-04-10 18:32:22 -0400

    Initial implementation of the Azure.AI resource provider.

---
+++ src/dotnet/Common/Models/ResourceProviders/Agent/AgentWorkflows/AzureAIAgentServiceWorkflow.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FoundationaLLM.Common.Models.ResourceProviders.Agent.AgentWorkflows
+{
+    /// <summary>
+    /// Provides an agent workflow configuration for an Azure AI Agent Service workflow.
+    /// </summary>
+    public class AzureAIAgentServiceWorkflow : AgentWorkflowBase
+    {
+        /// <inheritdoc/>
+        [JsonIgnore]
+        public override string Type => AgentWorkflowTypes.AzureAIAgentService;
+
+        /// <summary>
+        /// The Azure AI Agent ID for the agent workflow.
+        /// </summary>
+        [JsonPropertyName("agent_id")]
+        public string? AgentId { get; set; }
+
+        /// <summary>
+        /// The vector store ID for the agent.
+        /// </summary>
+        [JsonPropertyName("vector_store_id")]
+        public string? VectorStoreId { get; set; }
+    }
+}
