aa7469e6a83de6240c2d916b60da216d53eb8b92
Sensational Code <sensational.code@gmail.com>
2025-04-15 23:54:25 -0700

    Use tailwind classes instead of inline styles

--- src/ui/ManagementPortal/pages/security/role-assignments/index.vue
+++ src/ui/ManagementPortal/pages/security/role-assignments/index.vue
@@ -1,17 +1,17 @@
 <template>
 	<main id="main-content">
-		<div style="display: flex">
-			<div style="flex: 1">
+		<div class="flex">
+			<div class="flex-1">
 				<h2 class="page-header">Role Assignments</h2>
 				<div class="page-subheader">The following role assignments are available.</div>
 			</div>
 
-			<div style="display: flex; align-items: center">
+			<div class="flex items-center">
 				<NuxtLink to="/security/role-assignments/create" tabindex="-1">
 					<Button aria-label="Create Role Assignment">
 						<i
-							class="pi pi-plus"
-							style="color: var(--text-primary); margin-right: 8px"
+							class="pi pi-plus mr-2"
+							style="color: var(--text-primary)"
 							aria-hidden="true"
 						></i>
 						Create Role Assignment
