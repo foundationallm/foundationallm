f754d6b7f0a18ee3ca7e0f19a5377c46144ac89f
Sensational Code <sensational.code@gmail.com>
2025-04-16 22:54:16 -0700

    Use tailwind classes instead of inline styles

--- src/ui/ManagementPortal/pages/data-sources/index.vue
+++ src/ui/ManagementPortal/pages/data-sources/index.vue
@@ -1,15 +1,15 @@
 <template>
 	<main id="main-content">
-		<div style="display: flex">
-			<div style="flex: 1">
+		<div class="flex">
+			<div class="flex-1">
 				<h2 class="page-header">Data Sources</h2>
 				<div class="page-subheader">The following data sources are available.</div>
 			</div>
 
-			<div style="display: flex; align-items: center">
+			<div class="flex items-center">
 				<NuxtLink to="/data-sources/create" tabindex="-1">
 					<Button aria-label="Create data source">
-						<i class="pi pi-plus" style="color: var(--text-primary); margin-right: 8px"></i>
+						<i class="pi pi-plus mr-2" style="color: var(--text-primary)"></i>
 						Create Data Source
 					</Button>
 				</NuxtLink>
@@ -131,7 +131,7 @@
 				<Column
 					header="Delete"
 					header-style="width:6rem"
-					style="text-align: center"
+					class="text-center"
 					:pt="{
 						headerCell: {
 							style: { backgroundColor: 'var(--primary-color)', color: 'var(--primary-text)' },
