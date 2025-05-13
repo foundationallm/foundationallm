5f2f952b8fe0a8335915bae642e62b3a7daab118
Matthew Alan Gray <matthew.gray@hatboysoftware.com>
2025-05-13 18:39:33 -0500

    Standard deployment updates to accomodate pre-configured resource groups and service principal authentication

--- deploy/standard/azd-hooks/utility/Load-Utility-Functions.ps1
+++ deploy/standard/azd-hooks/utility/Load-Utility-Functions.ps1
@@ -7,6 +7,7 @@ try {
     . ./utility/Get-AbsolutePath.ps1
     . ./utility/Get-Resource-Suffix.ps1
     . ./utility/Invoke-AndRequireSuccess.ps1
+    . ./utility/Credential-Helpers.ps1
 }
 finally {
     Pop-Location
