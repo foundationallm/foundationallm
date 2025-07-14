8c7c3f8ef7d3b302e14ab28878d69447b31ce962
Ciprian Jichici <ciprian@solliance.net>
2025-07-14 16:06:01 +0300

    Improve the PowerShell module

--- automation/FoundationaLLM.Core/FoundationaLLM.Core.psd1
+++ automation/FoundationaLLM.Core/FoundationaLLM.Core.psd1
@@ -12,7 +12,7 @@
 RootModule = 'FoundationaLLM.Core.psm1'
 
 # Version number of this module.
-ModuleVersion = '0.0.9'
+ModuleVersion = '0.0.11'
 
 # Supported PSEditions
 CompatiblePSEditions = @(
@@ -73,7 +73,8 @@ PowerShellVersion = '7.0'
 
 # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
 FunctionsToExport = @(
-    'Deploy-FoundationaLLMPackage'
+    'Deploy-FoundationaLLMPackage',
+    'Import-Dotenv'
 )
 
 # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
--- automation/FoundationaLLM.Core/private/FoundationaLLM-Authorization.ps1
+++ automation/FoundationaLLM.Core/private/FoundationaLLM-Authorization.ps1
@@ -51,3 +51,59 @@ function Merge-RoleAssignment {
         -RelativeUri "providers/FoundationaLLM.Authorization/roleAssignments/$($RoleAssignment.name)" `
         -Body $RoleAssignment
 }
+
+function Merge-RoleAssignments {
+    param (
+        [string]$PackageRoot,
+        [hashtable]$Parameters = @{}
+    )
+
+    Test-RoleDefinitionIds
+
+    $roleAssignments = Get-Content "$($PackageRoot)/artifacts/roleAssignments.json" `
+        | Resolve-Placeholders -Parameters $Parameters `
+        | ConvertFrom-Json -AsHashTable
+
+    $roleAssignments | ForEach-Object {
+
+        $securityPrincipal = $_
+
+        if ($null -ne $securityPrincipal.principal_id){
+            $securityPrincipalId = $securityPrincipal.principal_id
+        } elseif ($securityPrincipal.principal_type -eq "User") {
+            $securityPrincipalId = Get-EntraUserId -UPN $securityPrincipal.principal_name
+        } else {
+            $securityPrincipalId = Get-EntraSecurityGroupId -Name $securityPrincipal.principal_name
+        }
+
+        Write-Host "Assigning roles for [$($securityPrincipal.principal_name)]($($securityPrincipalId))"
+
+        foreach ($roleAssignment in $securityPrincipal.role_assignments) {
+            $scope = Get-ObjectId -Name $roleAssignment[2] -Type $roleAssignment[1]
+            $roleDefinitionId = $global:RoleDefinitionIds[$roleAssignment[0]]
+            Write-Host "Assigning role [$($roleAssignment[0])]($($roleDefinitionId)) to $($scope)"
+
+            $roleAssignmentRequest = [ordered]@{
+                type = "FoundationaLLM.Authorization/roleAssignments"
+                name = (New-Guid).ToString("D")
+                description = "$($roleAssignment[0]) role for $($securityPrincipal.principal_name)"
+                principal_id = $securityPrincipalId
+                principal_type = $securityPrincipal.principal_type
+                role_definition_id = $roleDefinitionId
+                scope = $scope
+            }
+
+            try {
+                $roleAssignmentResponse = Merge-RoleAssignment -RoleAssignment $roleAssignmentRequest
+                Write-Host "Role assignment created: $($roleAssignmentResponse)" -ForegroundColor Green
+            }
+            catch [System.Net.Http.HttpRequestException] {
+                if ($_.Exception.Response.StatusCode -eq 409) {
+                    Write-Host "Role assignment already exists." -ForegroundColor Yellow
+                } else {
+                    throw $_
+                }
+            }
+        }
+    }
+}
