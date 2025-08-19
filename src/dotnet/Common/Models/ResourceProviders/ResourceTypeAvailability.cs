025411b571a95f7e984c5789715ab529681ad346
Ciprian Jichici <ciprian@foundationallm.ai>
2025-08-19 18:56:34 +0300

    Enable check name action in Core API management controller

---
+++ src/dotnet/Common/Models/ResourceProviders/ResourceTypeAvailability.cs
@@ -0,0 +1,23 @@
+﻿namespace FoundationaLLM.Common.Models.ResourceProviders
+{
+    /// <summary>
+    /// Provides details about the availability of a resource type in various API operations.
+    /// </summary>
+    public class ResourceTypeAvailability
+    {
+        /// <summary>
+        /// Gets or sets a flag that indicates whether the resource type itself is available.
+        /// </summary>
+        public bool IsResourceTypeAvailable { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of available actions for the resource type.
+        /// </summary>
+        public HashSet<string> AvailableActions { get; set; } = [];
+
+        /// <summary>
+        /// Gets or sets the list of available subordinate resource types.
+        /// </summary>
+        public Dictionary<string, ResourceTypeAvailability> AvailableSubordinateResourceTypes { get; set; } = [];
+    }
+}
--- src/dotnet/Core/Services/CoreResourcePathAvailabilityCheckerService.cs
+++ src/dotnet/Core/Services/CoreResourcePathAvailabilityCheckerService.cs
@@ -15,20 +15,40 @@ namespace FoundationaLLM.Core.Services
         ILogger<CoreResourcePathAvailabilityCheckerService> logger) : IResourcePathAvailabilityCheckerService
     {
         private readonly ILogger<CoreResourcePathAvailabilityCheckerService> _logger = logger;
-        private readonly Dictionary<HttpMethod, Dictionary<string, HashSet<string>>> _availabilityMap =
+        private readonly Dictionary<HttpMethod, Dictionary<string, Dictionary<string, ResourceTypeAvailability>>> _availabilityMap =
             new()
             {
-                [HttpMethod.Get] = new Dictionary<string, HashSet<string>>(StringComparer.OrdinalIgnoreCase)
+                [HttpMethod.Get] = new Dictionary<string, Dictionary<string, ResourceTypeAvailability>>(StringComparer.OrdinalIgnoreCase)
                 {
-                    [ResourceProviderNames.FoundationaLLM_AIModel] = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+                    [ResourceProviderNames.FoundationaLLM_AIModel] = new Dictionary<string, ResourceTypeAvailability>(StringComparer.OrdinalIgnoreCase)
                     {
-                        AIModelResourceTypeNames.AIModels
+                        [AIModelResourceTypeNames.AIModels] = new ResourceTypeAvailability
+                        {
+                            IsResourceTypeAvailable = true
+                        }
                     },
-                    [ResourceProviderNames.FoundationaLLM_Agent] = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+                    [ResourceProviderNames.FoundationaLLM_Agent] = new Dictionary<string, ResourceTypeAvailability>(StringComparer.OrdinalIgnoreCase)
                     {
-                        AgentResourceTypeNames.Agents
+                        [AgentResourceTypeNames.Agents] = new ResourceTypeAvailability
+                        {
+                            IsResourceTypeAvailable = true
+                        }
                     }
-                }
+                },
+                [HttpMethod.Post] = new Dictionary<string, Dictionary<string, ResourceTypeAvailability>>(StringComparer.OrdinalIgnoreCase)
+                {
+                    [ResourceProviderNames.FoundationaLLM_Agent] = new Dictionary<string, ResourceTypeAvailability>(StringComparer.OrdinalIgnoreCase)
+                    {
+                        [AgentResourceTypeNames.Agents] = new ResourceTypeAvailability
+                        {
+                            IsResourceTypeAvailable = false,
+                            AvailableActions = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+                            {
+                                ResourceProviderActions.CheckName
+                            }
+                        }
+                    }
+                },
             };
 
         /// <inheritdoc/>
@@ -38,10 +58,52 @@ namespace FoundationaLLM.Core.Services
         {
             try
             {
-                var isAvailable =
-                    _availabilityMap.TryGetValue(method, out var resourceProviders)
-                    && resourceProviders.TryGetValue(resourcePath.ResourceProvider!, out var resourceTypes)
-                    && resourceTypes.Contains(resourcePath.MainResourceTypeName!);
+                var isAvailable = true;
+
+                // Check if the method and the resource provider are registered in the availability map.
+                if (_availabilityMap.TryGetValue(method, out var resourceProviders)
+                    && resourceProviders.TryGetValue(resourcePath.ResourceProvider!, out var currentResourceTypesAvailability))
+                {
+                    foreach (var resourceTypeInstance in resourcePath.ResourceTypeInstances)
+                    {
+                        //  Check if the resource type is registered in the availability map.
+                        if (currentResourceTypesAvailability is null
+                            || !currentResourceTypesAvailability.TryGetValue(resourceTypeInstance.ResourceTypeName, out var resourceTypeAvailability))
+                        {
+                            isAvailable = false;
+                            break;
+                        }
+
+                        // NOTE: An action on a resource type can be available even if the resource type itself is not available.
+                        // This is to allow for actions that can be performed on a resource type that is not available for general use.
+
+                        bool hasAction = !string.IsNullOrWhiteSpace(resourceTypeInstance.Action);
+                        bool isActionAvailable =
+                            hasAction
+                            && resourceTypeAvailability.AvailableActions.Contains(resourceTypeInstance.Action!);
+
+                        // Check if the action is available.
+                        if (hasAction && !isActionAvailable)
+                        {
+                            isAvailable = false;
+                            break;
+                        }
+
+                        // Check if the resource type is available.
+                        // We only need to check the availability of the resource type if there is no action.
+                        // If an action is present it's either unavailable (already checked above) or available, and the resource type availability is not relevant.
+                        if (!hasAction
+                            && !resourceTypeAvailability.IsResourceTypeAvailable)
+                        {
+                            isAvailable = false;
+                            break;
+                        }
+
+                        currentResourceTypesAvailability = resourceTypeAvailability.AvailableSubordinateResourceTypes;
+                    }
+                }
+                else
+                    isAvailable = false;
 
                 if (!isAvailable)
                     _logger.LogWarning(
