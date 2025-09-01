309e2f8eb505658b4d07bd9c4e3da3c022a8d413
Ciprian Jichici <ciprian@foundationallm.ai>
2025-09-01 12:18:28 +0300

    Add role assignments for agents in Core API

---
+++ src/dotnet/Authorization/Validation/SecurityPrincipalQueryParametersValidator.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using FoundationaLLM.Common.Constants.Authentication;
+using FoundationaLLM.Common.Models.Authorization;
+
+namespace FoundationaLLM.Authorization.Validation
+{
+    /// <summary>
+    /// Validator for the <see cref="SecurityPrincipalQueryParameters"/> model.
+    /// </summary>
+    public class SecurityPrincipalQueryParametersValidator : AbstractValidator<SecurityPrincipalQueryParameters>
+    {
+        /// <summary>
+        /// Configures the validation rules for the <see cref="SecurityPrincipalQueryParameters"/> model.
+        /// </summary>
+        public SecurityPrincipalQueryParametersValidator()
+        {
+            RuleLevelCascadeMode = CascadeMode.Stop;
+
+            RuleFor(x => x)
+                .Must(x =>
+                {
+                    bool hasNameAndType = !string.IsNullOrWhiteSpace(x.Name) && !string.IsNullOrWhiteSpace(x.SecurityPrincipalType);
+                    bool hasIds = x.Ids != null && x.Ids.Length > 0;
+
+                    // Only one of the two options can be true
+                    return (hasNameAndType && !hasIds) || (hasIds && string.IsNullOrWhiteSpace(x.Name) && string.IsNullOrWhiteSpace(x.SecurityPrincipalType));
+                })
+                .WithMessage("Specify either both Name and SecurityPrincipalType, or Ids, but not both.");
+
+
+            RuleFor(x => x.SecurityPrincipalType)
+                .Must(type => string.IsNullOrWhiteSpace(type) || SecurityPrincipalTypes.All.Contains(type))
+                .WithMessage("SecurityPrincipalType must be one of the allowed values.");
+        }
+    }
+}
--- src/dotnet/Common/Models/Authorization/SecurityPrincipalQueryParameters.cs
+++ src/dotnet/Common/Models/Authorization/SecurityPrincipalQueryParameters.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using FoundationaLLM.Common.Constants.Authentication;
+using System.Text.Json.Serialization;
 
 namespace FoundationaLLM.Common.Models.Authorization
 {
@@ -8,9 +9,33 @@ namespace FoundationaLLM.Common.Models.Authorization
     public class SecurityPrincipalQueryParameters
     {
         /// <summary>
-        /// Gets of sets the list of security principal identifiers to query.
+        /// Gets or sets the name of the security principal to query.
         /// </summary>
+        /// <remarks>
+        /// The name can be a partial name used to indetify multiple security principals.
+        /// When this property is specified, the SecurityPrincipalName property muse be set
+        /// and the Ids property must be an empty array.
+        /// </remarks>
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of security principal to query.
+        /// Must be one of the <see cref="SecurityPrincipalTypes"/> values.
+        /// </summary>
+        /// <remarks>
+        /// This property must be set when the Name property is specified.
+        /// </remarks>
+        [JsonPropertyName("security_principal_type")]
+        public string? SecurityPrincipalType { get; set; }
+
+        /// <summary>
+        /// Gets of sets a list of security principal identifiers to query.
+        /// </summary>
+        /// <remarks>
+        /// When this property is specified, the Name and SecurityPrincipalType properties must be null.
+        /// </remarks>
         [JsonPropertyName("ids")]
-        public required string[] Ids { get; set; }
+        public string[]? Ids { get; set; }
     }
 }
