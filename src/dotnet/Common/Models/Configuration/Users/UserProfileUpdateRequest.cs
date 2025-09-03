828d2b0f489e818ae15dfa7855a35e720b44661a
Ciprian Jichici <ciprian@foundationallm.ai>
2025-09-03 11:17:31 +0300

    Persist agent selection in user profile

---
+++ src/dotnet/Common/Models/Configuration/Users/UserProfileUpdateRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FoundationaLLM.Common.Models.Configuration.Users
+{
+    /// <summary>
+    /// Represents a request to update the properties of a user profile.
+    /// </summary>
+    public class UserProfileUpdateRequest
+    {
+        /// <summary>
+        /// Gets or sets the object identifier of the agent to add or remove from the user's profile. 
+        /// </summary>
+        [JsonPropertyName("agent_object_id")]
+        public string? AgentObjectId { get; set; }
+    }
+}
