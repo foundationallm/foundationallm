973ba20eb44415a2104e0992fde845d8301a406d
Ciprian Jichici <ciprian@foundationallm.ai>
2025-11-05 14:24:09 +0200

    Deploy Orchestration API artifacts

--- deploy/internal/FoundationaLLM.Deployment.Internal/private/02-agents/scripts/orchestration-api.ps1
+++ deploy/internal/FoundationaLLM.Deployment.Internal/private/02-agents/scripts/orchestration-api.ps1
@@ -68,3 +68,34 @@ function Restart-OrchestrationAPI {
         -ResourceGroupName $resourceGroupNames.Core `
         -ContainerAppName $resourceNames.OrchestrationAPIContainerApp
 }
+
+
+function New-OrchestrationAPIArtifacts {
+    param (
+        [string]$UniqueName,
+        [string]$InstanceId,
+        [string]$UserGroupObjectId
+    )
+
+    $resourceNames = Get-ResourceNames -UniqueName $UniqueName
+    $resourceGroupNames = Get-ResourceGroupNames -UniqueName $UniqueName
+
+    $managementAPIEndpointURL = "https://" + (az containerapp ingress show -n $resourceNames.ManagementAPIContainerApp -g $resourceGroupNames.Core --query "fqdn" -o tsv)
+    $coreAPIEndpointURL = "https://" + (az containerapp ingress show -n $resourceNames.CoreAPIContainerApp -g $resourceGroupNames.Core --query "fqdn" -o tsv)
+
+    # Initialize global variables for the FoundationaLLM.Core module
+    $global:InstanceId = $InstanceId
+    $global:ManagementAPIBaseUrl = $managementAPIEndpointURL
+    $global:ManagementAPIInstanceRelativeUri = "/instances/$($global:InstanceId)"
+    $global:CoreAPIBaseUrl = $coreAPIEndpointURL
+    $global:CoreAPIInstanceRelativeUri = "/instances/$($global:InstanceId)"
+
+    $orchestrationAPIManagedIdentityObjectId = (az identity show -n $resourceNames.OrchestrationAPIManagedIdentity -g $resourceGroupNames.Core --query principalId -o tsv)
+    $packagePath = "$PSScriptRoot\..\packages\orchestration-api"
+
+    Deploy-FoundationaLLMPackage `
+        -PackageRoot $packagePath `
+        -Parameters @{
+            ORCHESTRATION_API_MI_OBJECT_ID = $orchestrationAPIManagedIdentityObjectId
+        }
+}
