09afe9873b5c0c06acc6f4780bec1c49ac507d8c
ZoinerTejada <ZoinerTejada@users.noreply.github.com>
2026-01-07 13:05:04 +0000

    deploy: 602f4b7b3eb6ad49f772e51b6937fb2423979267

--- public/main.js
+++ public/main.js
@@ -1,10 +1,1 @@
-export default {
-    defaultTheme: 'light',
-    iconLinks: [
-      {
-        icon: 'github',
-        href: 'https://github.com/solliancenet/foundationallm',
-        title: 'GitHub'
-      }
-    ]
-  }
+export default {}
